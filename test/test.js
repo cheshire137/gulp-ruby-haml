fac50dd429d1bdd7832009ebbe4d1520e4273089
Sarah Vessels <sarah.vessels@cirrusmio.com>
2014-02-19 13:26:45 -0500

    Add tests for invalid Haml compilation

--- test/test.js
+++ test/test.js
@@ -34,3 +34,30 @@ describe('basic Haml conversion', function() {
     });
   });
 });
+
+describe('it throws error on invalid Haml', function() {
+  var in_path = path.join(fixture_dir, 'invalid.haml');
+  var error = null;
+
+  gulp.task('invalid-haml', function() {
+    return gulp.src(in_path).
+                pipe(haml().on('error', function (err) { error = err; })).
+                pipe(gulp.dest(dest_dir));
+  });
+
+  it('does not compile invalid Haml into HTML', function (done) {
+    var out_path = path.join(dest_dir, 'invalid.html');
+    gulp.run('invalid-haml', function() {
+      assert.equal(fs.existsSync(out_path), false,
+                   'Expected ' + out_path + ' to not exist');
+      done();
+    });
+  });
+
+  it('emits an error', function (done) {
+    gulp.run('invalid-haml', function() {
+      assert(error.message.indexOf('Syntax error') > -1);
+      done();
+    });
+  });
+});
